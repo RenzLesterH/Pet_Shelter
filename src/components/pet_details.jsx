0ed4ffc71dfbce022e9fe3adae0b91f58b9f8188
Renz <ako2renz@gmail.com>
2022-10-21 14:02:06 +0800

    Rendered the json file in pet_details component.

--- src/components/pet_details.jsx
+++ src/components/pet_details.jsx
@@ -2,44 +2,25 @@ import React, { Component } from 'react';
 import './stylesheets/styles.scss';
 
 class Pet_details extends Component {
-    // state = {  } 
-    render() { 
+    state = {
+        pets_data: this.props.pets_data
+     } 
+    render() {
+        let { pets_data } = this.state;
         return (
             <main>
                 <h3>These pets are looking for a good home</h3>
                 <table>
+                {pets_data.map((pets) => (
                     <tr>
-                        <td className="pet_name">Garfied</td>
-                        <td class="pet_type">Cat</td>
+                        <td className="pet_name" key={pets.name}>{pets.name}</td>
+                        <td class="pet_type" key={pets.type}>{pets.type}</td>
                         <td>
                             <button><i class="fas fa-clipboard-list"></i>Details</button>
                             <button><i className="fas fa-pen-square"></i>Edit</button>
                         </td>
                     </tr>
-                    <tr>
-                        <td className="pet_name">Doraemon</td>
-                        <td class="pet_type">Cat</td>
-                        <td>
-                            <button><i class="fas fa-clipboard-list"></i>Details</button>
-                            <button><i className="fas fa-pen-square"></i>Edit</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="pet_name">Snoopy</td>
-                        <td class="pet_type">Dog</td>
-                        <td>
-                            <button><i class="fas fa-clipboard-list"></i>Details</button>
-                            <button><i className="fas fa-pen-square"></i>Edit</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="pet_name">Dug</td>
-                        <td class="pet_type">Dog</td>
-                        <td>
-                            <button><i class="fas fa-clipboard-list"></i>Details</button>
-                            <button><i className="fas fa-pen-square"></i>Edit</button>
-                        </td>
-                    </tr>
+                ))}
                 </table>
             </main>
         );
--- src/pages/home.js
+++ src/pages/home.js
@@ -5,15 +5,19 @@ import Navbar from "../components/navbar";
 import Banner from "../components/banners";
 import Main from "../components/pet_details";
 import Footer from "../components/footer";
+import Records from "../pet_info.json";
 
 class Home extends Component {
-  // state = {  } 
+  state = { 
+    pets: Records
+  } 
   render() {
+    let { pets } = this.state;
     return (
       <React.Fragment>
         <Navbar />
         <Banner/>
-        <Main />
+        <Main pets_data={pets}/>
         <Footer />
       </React.Fragment>
     );
