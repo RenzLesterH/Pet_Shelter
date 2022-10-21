cd19924ed262a349d53b45af5493ea7fc9e75027
Renz <ako2renz@gmail.com>
2022-10-21 09:14:30 +0800

    In Navbar back to home is added as hidden first.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -11,10 +11,11 @@ class Navbar extends Component {
             <header>
                 <nav>
                   <ul>
+                      <li id="back_to_home"><a href="#">Back to Home</a></li>
                       <li id="brand_name"><a href='#'>PETSHELTER</a></li>
-                      <li><a href='#'>Home</a></li>
-                      <li><a href='#'>Services</a></li>
-                      <li><a href='#'>Events</a></li>
+                      <li class="nav_links"><a href='#'>Home</a></li>
+                      <li class="nav_links"><a href='#'>Services</a></li>
+                      <li class="nav_links"><a href='#'>Events</a></li>
                       <li id="add_pet_to_shelter"><a href='#'><i class="fas fa-plus-circle"></i>Add pet to Shelter</a></li>
                   </ul>
                 </nav>
