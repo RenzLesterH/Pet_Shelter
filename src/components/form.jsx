2d89f4af6221755a0b2c8ebf58128e5fddb2adfd
Renz <ako2renz@gmail.com>
2022-10-21 14:08:48 +0800

    Edited form and pet_json file

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -5,14 +5,14 @@ class Form extends Component {
     render() { 
         return (
             <section id="add_pet">
-                <form action="">
+                <form method="post">
                     <div>
                         <label>Pet Name</label>
-                        <input id="pet_name_input" type="text" name="" />
+                        <input id="pet_name_input" type="text" name="pet_name" required/>
                     </div>
                     <div>
                        <label>Pet Type</label> 
-                        <select id="pet_type_select">
+                        <select id="pet_type_select" name="pet_type" required>
                             <option>Pig</option>
                             <option>Dog</option>
                             <option>Cat</option>
@@ -20,13 +20,13 @@ class Form extends Component {
                     </div>
                     <div>
                         <label id="pet_desc_label">Description</label>
-                        <textarea name="" id="pet_desc_textarea"></textarea>
+                        <textarea name="pet_desc" id="pet_desc_textarea" required></textarea>
                     </div>
                     <div>
                         <label>Skills</label>
-                        <input type="text" name="" id="skill_1" />
-                        <input type="text" class="pet_skills" name="" />
-                        <input type="text" class="pet_skills" name="" />
+                        <input type="text" name="pet_skill_1" id="skill_1" required />
+                        <input type="text" name="pet_skill_2" class="pet_skills" required />
+                        <input type="text" name="pet_skill_3" class="pet_skills" required />
                     </div>
                     <button type="submit"><i class="fas fa-upload"></i>Add Pet</button>
                 </form>
