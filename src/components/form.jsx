a90de09b3db5140bf9886cb40fbf655e321b0b94
Renz Lester J. Halagao <halagao72@gmail.com>
2022-10-25 03:07:10 +0800

    onchang in form inputs was implemented.

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -3,62 +3,139 @@ import './stylesheets/styles.scss';
 import './stylesheets/form_styles.scss';
 
 class Form extends Component {
-    // state = {  }
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            pet_details: {
+                id: this.props.setId+1,
+                name: null,
+                type: "Pig",
+                description: null,
+                skill_1: null,
+                skill_2: null,
+                skill_3: null,
+                likes: 0,
+            }
+        }
+    }
     
     handleOnSubmit = (event) => {
-        event.preventDefault();
-        
-        let data_form = event.target;
-
-        this.props.handleOnClick({
-          id: this.props.setId+1,
-          name: data_form.pet_name.value,
-          type: data_form.pet_type.value,
-          description: data_form.pet_desc.value,
-          skill_1: data_form.pet_skill_1.value,
-          skill_2: data_form.pet_skill_2.value,
-          skill_3: data_form.pet_skill_3.value,
-          likes: 0,
-        });
-
-        data_form.pet_name.value = "";
-        data_form.pet_type.value = "";  
-        data_form.pet_desc.value = "";
-        data_form.pet_skill_1.value = "";
-        data_form.pet_skill_2.value = "";
-        data_form.pet_skill_3.value = "";
+        event.preventDefault();;
+        this.props.handleOnClick(this.state.pet_details);
+        event.target.reset();
         this.props.showToast();
     };
 
     render() { 
         return (
-            <section id="add_pet">
-                <form method="post" onSubmit={this.handleOnSubmit}>
-                    <div>
-                        <label>Pet Name</label>
-                        <input id="pet_name_input" type="text" name="pet_name" required/>
-                    </div>
-                    <div>
-                       <label>Pet Type</label> 
-                        <select id="pet_type_select" name="pet_type" required>
-                            <option>Pig</option>
-                            <option>Dog</option>
-                            <option>Cat</option>
-                        </select>
-                    </div>
-                    <div>
-                        <label id="pet_desc_label">Description</label>
-                        <textarea name="pet_desc" id="pet_desc_textarea" required></textarea>
-                    </div>
-                    <div>
-                        <label>Skills</label>
-                        <input type="text" name="pet_skill_1" id="skill_1" required />
-                        <input type="text" name="pet_skill_2" className="pet_skills" required />
-                        <input type="text" name="pet_skill_3" className="pet_skills" required />
-                    </div>
-                    <button type="submit"><i className="fas fa-upload"></i>Add Pet</button>
-                </form>
-            </section>
+          <section id="add_pet">
+            <form method="post" onSubmit={this.handleOnSubmit}>
+              <div>
+                <label>Pet Name</label>
+                <input
+                  id="pet_name_input"
+                  type="text"
+                  name="pet_name"
+                  onChange={(event) =>
+                    this.setState({
+                      pet_details: {
+                        ...this.state.pet_details,
+                        name: event.target.value,
+                      },
+                    })
+                  }
+                  required
+                />
+              </div>
+              <div>
+                <label>Pet Type</label>
+                <select
+                  id="pet_type_select"
+                  name="pet_type"
+                  onChange={(event) =>
+                    this.setState({
+                      pet_details: {
+                        ...this.state.pet_details,
+                        type: event.target.value,
+                      },
+                    })
+                  }
+                  required
+                >
+                  <option>Pig</option>
+                  <option>Dog</option>
+                  <option>Cat</option>
+                </select>
+              </div>
+              <div>
+                <label id="pet_desc_label">Description</label>
+                <textarea
+                  name="pet_desc"
+                  id="pet_desc_textarea"
+                  onChange={(event) =>
+                    this.setState({
+                      pet_details: {
+                        ...this.state.pet_details,
+                        description: event.target.value,
+                      },
+                    })
+                  }
+                  required
+                >
+                  {" "}
+                </textarea>
+              </div>
+              <div>
+                <label>Skills</label>
+                <input
+                  type="text"
+                  name="pet_skill_1"
+                  id="skill_1"
+                  onChange={(event) =>
+                    this.setState({
+                      pet_details: {
+                        ...this.state.pet_details,
+                        skill_1: event.target.value,
+                      },
+                    })
+                  }
+                  required
+                />
+                <input
+                  type="text"
+                  name="pet_skill_2"
+                  className="pet_skills"
+                  onChange={(event) =>
+                    this.setState({
+                      pet_details: {
+                        ...this.state.pet_details,
+                        skill_2: event.target.value,
+                      },
+                    })
+                  }
+                  required
+                />
+                <input
+                  type="text"
+                  name="pet_skill_3"
+                  className="pet_skills"
+                  onChange={(event) =>
+                    this.setState({
+                      pet_details: {
+                        ...this.state.pet_details,
+                        skill_3: event.target.value,
+                      },
+                    })
+                  }
+                  required
+                />
+              </div>
+              <button type="submit">
+                <i className="fas fa-upload"></i>Add Pet
+              </button>
+            </form>
+          </section>
         );
     }
 }
