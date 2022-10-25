0eacd368a196f8567864094d859c12022d19bd12
Renz <ako2renz@gmail.com>
2022-10-25 16:11:29 +0800

    Link in add pet in edit pet is fixed.

--- src/components/navbar_add_pet.jsx
+++ src/components/navbar_add_pet.jsx
@@ -13,7 +13,7 @@ class Navbar extends Component {
                   <ul>
                       <li id="back_to_home"><Link to='/'><i className="far fa-arrow-left"></i>Back to Home</Link></li>
                       <li id="add_pet_brand_name"><Link to='/'>PETSHELTER</Link></li>
-                      <li id="add_pet_to_shelter"><Link to='add-pet'><i className="fas fa-plus-circle"></i>Add pet to Shelter</Link></li>
+                      <li id="add_pet_to_shelter"><Link to='../add-pet'><i className="fas fa-plus-circle"></i>Add pet to Shelter</Link></li>
                   </ul>
                 </nav>
             </header>
